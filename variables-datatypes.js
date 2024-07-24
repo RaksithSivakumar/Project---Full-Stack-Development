b7e8f54bb7bb6790d7667200bc437498d2b3224b
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-07-24 18:38:19 +0530

    Variables and Data Types in JavaScript
    A quick overview of how to use var, let, and const to declare variables and the different data types supported in JavaScript.

---
+++ variables-datatypes.js
@@ -0,0 +1,23 @@
+// Variables
+
+// Using var
+var name = "Risivan";
+
+// Using let
+let age = 20;
+
+// Using const
+const country = "India";
+
+// Data types
+
+let number = 55;          // Number
+let text = "Hello";       // String
+let isTrue = true;        // Boolean
+let person = {            // Object
+  firstName: "Risivan",
+  lastName: "Arjun"
+};
+let colors = ["Red", "Green", "Blue"];  // Array
+let undefinedVar;         // Undefined
+let nullVar = null;       // Null
