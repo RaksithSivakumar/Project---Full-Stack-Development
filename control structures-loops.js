09e8c04553b86435334d7172800b16fd24e5353d
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-07-27 17:57:23 +0530

    JavaScript Control Structures and Loops
    How to efficiently use if-else statements and various loops to make decisions and repeat code blocks

---
+++ control structures-loops.js
@@ -0,0 +1,35 @@
+// Contorl Structure
+
+let age = 66;
+
+if (age < 18) {
+    console.log("Minor");
+} 
+else if (age >= 18 && age < 65) {
+    console.log("Adult");
+}
+else {
+    console.log("sensior")
+}
+
+// Loops
+
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// while loop
+let i = 10;
+while (i < 20) {
+    console.log(i);
+    i++;
+}
+
+// do-while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 5);
+
