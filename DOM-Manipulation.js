e20dcb0331f73de3a3a03612686124ea8f2052c0
RaksithSivakumar <167526590+RaksithSivakumar@users.noreply.github.com>
2024-07-29 18:27:21 +0530

    Javascript DOM Manipulation
    Learn to select elements, change content and style, and add event listeners.

---
+++ DOM-Manipulation.js
@@ -0,0 +1,10 @@
+let element = document.getElementById("myElement");
+
+element.innerHTML = "Hello World";
+
+element.style.color = "red";
+
+
+element.addEventListener("click", function() {
+  alert("Element clicked!");
+});
